c57514d64800415fe786f1fa4338376fe5a07a38
David Hanina <dhanina@redhat.com>
2026-02-06 20:48:29 +0100

    Unify the style of add buttons
    All add buttons should now have primary action set properly and are of
    the correct size.
    
    Signed-off-by: David Hanina <dhanina@redhat.com>

--- src/components/modals/UserModals/AddUser.tsx
+++ src/components/modals/UserModals/AddUser.tsx
@@ -506,7 +506,6 @@ const AddUser = (props: PropsToAddUser) => {
     <Button
       data-cy="modal-button-add"
       key="add-new-user"
-      variant="secondary"
       isDisabled={buttonDisabled || addSpinning}
       type="submit"
       form="users-add-user-modal"
